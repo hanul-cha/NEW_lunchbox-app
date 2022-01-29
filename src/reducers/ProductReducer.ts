9fd13fc4c7c53d9bb9d0b8761bc888cb2e67cc1c
hanul-cha <ckgks3014@naver.com>
2022-01-29 15:06:49 +0900

    ssr에서 redux쓰는법

--- src/reducers/ProductReducer.ts
+++ src/reducers/ProductReducer.ts
@@ -1,10 +1,17 @@
-import { ProductDispatchType, PRODUCT_FAIL, PRODUCT_SUCCESS } from "../actions/ProductActionType"
+import { ProductDispatchType, PRODUCT_FAIL, PRODUCT_SUCCESS, ProductType } from "../actions/ProductActionType"
 
-const initialState = {
+export interface ISuccessState {
+    success : boolean
+    productLIst? : ProductType[]
+}
+
+const initialState :ISuccessState = {
     success:false
 }
 
-const ProductReducer = (state = initialState, action:ProductDispatchType) => {
+
+
+const ProductReducer = (state: ISuccessState = initialState, action:ProductDispatchType) => {
     switch (action.type) {
         case PRODUCT_FAIL:
             return{
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,22 @@
-import { combineReducers } from "redux";
-import ProductReducer from "./ProductReducer";
+import { AnyAction, CombinedState, combineReducers } from "redux";
+import ProductReducer, { ISuccessState } from "./ProductReducer";
 import { HYDRATE } from "next-redux-wrapper"
 
-const rootReducer = combineReducers({
-    
-    ProductReducer
-})
+const rootReducer = (state: IState, action: AnyAction) : CombinedState<IState> => {
+    switch (action.type) {
+        case HYDRATE:
+            return action.payload;
+        default: {
+            const combineReducer = combineReducers({
+                ProductReducer
+            });
+            return combineReducer(state, action)
+        }
+    }
+}
+
+export interface IState {
+    ProductReducer: never
+}
 
 export default rootReducer
