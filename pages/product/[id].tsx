0c122d7b1cace3711eff0b212a72286008d051a6
hanul-cha <ckgks3014@naver.com>
2022-01-29 22:32:44 +0900

    restFulAPI쓰는게 훨씬 안복잡하고 편함 리덕스는 장바구니 기능에서 사용하면 될듯하다

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,31 @@
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductData } from "../../src/actions/ProductAction";
 import { RootReducerType } from "../../src/Store";
 
-const Product = () => {
+
+interface productListType {
+  explanation: string;
+  img: string;
+  name: string;
+  new_product: boolean;
+  price: number;
+  product_id: number;
+  product_type: string;
+} //제품에 할당된 타입
+
+interface allProductType {
+  product: {
+    productList: productListType[];
+  };
+} //현재 페이지 props type
+
+const Product:NextPage<allProductType> = ({product}) => {
   const router = useRouter();
   const { id } = router.query;
-  const testReducer = useSelector((state:RootReducerType) =>state.ProductReducer)
+  /* const testReducer = useSelector((state:RootReducerType) =>state.ProductReducer)
   const dispatch = useDispatch()
 
   
@@ -16,7 +34,11 @@ const Product = () => {
     
   },[])
 
-  console.log(testReducer)
+  console.log(testReducer) */
+
+  const productInfo = product.productList[0].explanation
+
+  console.log(productInfo)
  
 
 
@@ -24,3 +46,16 @@ const Product = () => {
 };
 
 export default Product;
+
+
+export const getServerSideProps:GetServerSideProps = async (context) => {
+  const id = context.params?.id
+  const product = await (
+    await fetch(`http://localhost:3000/api/detailProduct/${id}`)
+  ).json();
+  return {
+    props: {
+      product,
+    },
+  };
+};
