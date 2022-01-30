e65012963556fa718a44d222c8a8e4ddeca463d1
hanul-cha <ckgks3014@naver.com>
2022-01-30 12:42:06 +0900

    ui컬러 미적용

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,10 +1,16 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductData } from "../../src/actions/ProductAction";
 import { RootReducerType } from "../../src/Store";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from "@mui/material";
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 interface productListType {
   explanation: string;
@@ -22,9 +28,8 @@ interface allProductType {
   };
 } //현재 페이지 props type
 
-const Product:NextPage<allProductType> = ({product}) => {
-  const router = useRouter();
-  const { id } = router.query;
+const Product: NextPage<allProductType> = ({ product }) => {
+  const [productCount, setProductCount] = useState(1);
   /* const testReducer = useSelector((state:RootReducerType) =>state.ProductReducer)
   const dispatch = useDispatch()
 
@@ -36,20 +41,85 @@ const Product:NextPage<allProductType> = ({product}) => {
 
   console.log(testReducer) */
 
-  const productInfo = product.productList[0].explanation
+  const productInfo = product.productList[0];
 
-  console.log(productInfo)
- 
+  console.log(productInfo);
 
-
-  return <div>post:{id}</div>;
+  return (
+    <>
+      <div className="productCard">
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={productInfo.img}
+              alt="green iguana"
+            />
+            <CardContent>
+              <div>
+                <h2>{productInfo.name}</h2>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className="productOption">
+          <h3>가격 : {productInfo.price}원</h3>
+          <div className="countWrapper">
+            <h3>수량</h3>
+            <div className="addTable">
+              <div>
+                <RemoveCircleIcon />
+              </div>
+              <h3 className="addTableCount">{productCount}개</h3>
+              <div>
+                <AddCircleIcon />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .productCard {
+          margin: 20px;
+        }
+        .productCard h2 {
+          font-size: 20px;
+          font-weight: 600;
+        }
+        .productCard h3 {
+          font-size: 20px;
+        }
+        .countWrapper {
+          display: grid;
+          justify-content: space-between;
+          grid-template-columns: auto 120px;
+          align-items: center;
+        }
+        
+        
+        
+        .addTable {
+          border: 1px solid #434343;
+          border-radius: 50px;
+          height: 50px;
+          display: grid;
+          align-items: center;
+          grid-template-columns: 40px 40px 40px;
+          
+        }
+        .addTable > * {
+          margin: 0 auto;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Product;
 
-
-export const getServerSideProps:GetServerSideProps = async (context) => {
-  const id = context.params?.id
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params?.id;
   const product = await (
     await fetch(`http://localhost:3000/api/detailProduct/${id}`)
   ).json();
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,18 +2,9 @@ import { AnyAction, CombinedState, combineReducers } from "redux";
 import ProductReducer, { ISuccessState } from "./ProductReducer";
 import { HYDRATE } from "next-redux-wrapper"
 
-const rootReducer = (state: IState, action: AnyAction) : CombinedState<IState> => {
-    switch (action.type) {
-        case HYDRATE:
-            return action.payload;
-        default: {
-            const combineReducer = combineReducers({
-                ProductReducer
-            });
-            return combineReducer(state, action)
-        }
-    }
-}
+const rootReducer = combineReducers({
+    ProductReducer
+})
 
 export interface IState {
     ProductReducer: never
